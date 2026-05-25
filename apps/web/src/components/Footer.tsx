426fcca08fae00935b111417e2c0fae9da688800
Eduardo Schröder <dudu.schroder@gmail.com>
2026-05-25 09:53:07 -0300

    chore: credits

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -20,6 +20,7 @@ export function Footer() {
         ))}
       </div>
       <div className="footer__credit">{t('footer.credit')}</div>
+      <div className='footer__author'>Eduardo Schröder</div>
     </footer>
   )
 }
